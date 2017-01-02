bd6f8836a479efd837f86aedceb4314061eb054c
hazim1093 <hazim_malik@hotmail.com>
2017-01-02 11:50:47 +0500

    Update start script

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # expects to find a logstash.ctmpl file in /templates. This is the template that we would
 # mount as a volume from our host. The result is then placed in /config-dir/logstash.conf
 # where logstash will be able to read from.
-logstash -r -f /etc/logstash/conf.d/logstash.conf
-consul-template -consul=$CONSUL_URL -template="/templates/logstash.ctmpl:/etc/logstash/conf.d/logstash.conf"
+logstash -f /config-dir/logstash.conf
+consul-template -consul=$CONSUL_URL -template="/templates/logstash.ctmpl:/config-dir/logstash.conf:service logstash restart"
