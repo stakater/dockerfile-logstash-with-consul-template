b439629e49d573285b8a2521bfcd8aaabc318ef3
hazim1093 <hazim_malik@hotmail.com>
2017-01-02 10:55:24 +0500

    Run logstash as user `logstash` with gosu

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # expects to find a logstash.ctmpl file in /templates. This is the template that we would
 # mount as a volume from our host. The result is then placed in /config-dir/logstash.conf
 # where logstash will be able to read from.
-logstash -f /config-dir/logstash.conf -r
-consul-template -consul=$CONSUL_URL -template="/templates/logstash.ctmpl:/config-dir/logstash.conf:logstash -f /config-dir/logstash.conf -r"
+gosu logstash logstash -f /config-dir/logstash.conf -r
+consul-template -consul=$CONSUL_URL -template="/templates/logstash.ctmpl:/config-dir/logstash.conf:gosu logstash logstash -f /config-dir/logstash.conf -r"
